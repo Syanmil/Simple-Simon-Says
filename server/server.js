3f573d4b5456a3994607b0ac04e4c25a626c1ebc
Syanmil <syanmil@yahoo.com>
2017-02-11 18:27:45 +0700

    red blue green color change

--- server/server.js
+++ server/server.js
@@ -4,9 +4,20 @@ var app = express()
 
 app.use(cors())
 
-app.get('/', function(req, res) {
-  res.send('oke')
+app.get('/api/color', function(req, res) {
+  res.send(red.blue('green'))
 })
+
+let red = {
+  blue: function() {
+    let randomNine = Math.ceil(Math.random()*9)
+    let randomred = Math.ceil(Math.random()*255)
+    let randomblue = Math.ceil(Math.random()*255)
+    let randomgreen = Math.ceil(Math.random()*255)
+    let color = `rgb(${randomred},${randomblue},${randomgreen})`
+    return {box: '#box'+randomNine, color: color}
+  }
+}
 
 app.listen(3000, function(){
   console.log('Connected at port 3000');
